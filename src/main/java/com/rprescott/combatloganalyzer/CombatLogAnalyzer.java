0e3b196629f9ae57f09496e7237cf7a1095083aa
Robert Prescott <rprescottjr@gmail.com>
2020-05-15 19:45:34 -0400

    Added missing import

--- src/main/java/com/rprescott/combatloganalyzer/CombatLogAnalyzer.java
+++ src/main/java/com/rprescott/combatloganalyzer/CombatLogAnalyzer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
